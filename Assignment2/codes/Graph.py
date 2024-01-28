347c609f7b4436060d1417a3757e90dfc56e0ee4
root <root@localhost.localdomain>
2024-01-28 16:36:21 +0000

    updated code

--- Assignment2/codes/Graph.py
+++ Assignment2/codes/Graph.py
@@ -1,57 +1,17 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
-# Given values of first AP
-a = 3  # first term
-d = 4  # common difference
-n = 15  # number of terms
+# Read data from the file
+with open('data.dat', 'r') as file:
+    data = [line.split() for line in file.readlines()]
 
-# Generate the arithmetic progression using NumPy
-ap_sequence = np.arange(a, a + n * d, d)
+# Extract n_values and x_values2
+n_values = [int(row[0]) for row in data]
+x_values2 = [int(row[1]) for row in data]
 
-# Calculate the sum of the first 15 terms using np.sum
-sum_an1 = np.sum(ap_sequence)
-
-# Given values of Second AP
-a = 9  # first term
-d = -5  # common difference
-n = 15  # number of terms
-
-# Generate the arithmetic progression using NumPy
-ap_sequence = np.arange(a, a + n * d, d)
-
-# Calculate the sum of the first 15 terms using np.sum
-sum_an2 = np.sum(ap_sequence)
-
-# Function to calculate x(n) = (3 + 4n)u(n)
-def x_n1(n):
-    return (3 + 4 * n) * (n >= 0)
-
-# Function to calculate x(n) = (9 - 5n)u(n)
-def x_n2(n):
-    return (9 - 5 * n) * (n >= 0)
-
-# Generate values for n
-n_values = np.arange(-10, 11, 1)
-
-# Calculate corresponding values for both x(n) signals
-x_values1 = x_n1(n_values)
-x_values2 = x_n2(n_values)
-
-# Plot the first signal
-plt.stem(n_values, x_values1, linefmt='b-', markerfmt='bo', basefmt='b')
-plt.xlabel('$n$')
-plt.ylabel('$x_1(n)$')
-plt.title('Discrete Signal $x_1(n) = (3 + 4n)u(n)$')
-plt.grid(True)
-plt.savefig('signal_x1.png')
-plt.clf()  # Clear the figure for the next plot
-
-# Plot the second signal
+# Plot the graph
 plt.stem(n_values, x_values2, linefmt='b-', markerfmt='bo', basefmt='b')
 plt.xlabel('$n$')
 plt.ylabel('$x_2(n)$')
 plt.title('Discrete Signal $x_2(n) = (9 - 5n)u(n)$')
 plt.grid(True)
-plt.savefig('signal_x2.png')
-plt.clf()  # Clear the figure
+plt.show()
