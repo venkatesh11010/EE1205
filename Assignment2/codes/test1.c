426302b67e23906fec20db7e641b6563638c69e1
venky <venkateshbandawar@gmail.com>
2024-01-30 10:22:06 +0530

    final commit

--- Assignment2/codes/test1.c
+++ Assignment2/codes/test1.c
@@ -9,6 +9,14 @@ int x_n1(int n) {
     return (3 + 4 * n) * (n >= 0);
 }
 
+int y_n1(int n) {
+    return (2 * n * n + 5 * n + 3) * (n >=0);
+}
+
+int y_n2(int n) {
+    return ((-5 * n * n + 13 * n + 18) / 2) * (n >=0);
+}
+
 int main() {
     int sum1=0;
     int sum2=0;
@@ -20,21 +28,22 @@ int main() {
     }
 
     // Generate values for n
-    for (int n = -5; n <= 14; ++n) {
+    for (int n = -5; n <= 15; ++n) {
         // Calculate corresponding values for x(n)
         int x_value1 = x_n1(n);
         int x_value2 = x_n2(n);
+        int y_value1 = y_n1(n);
+        int y_value2 = y_n2(n);
 
         //calculating sums
         sum1 += x_value1;
         sum2 += x_value2;
 
         // Write values to the file
-        fprintf(file, "%d %d %d\n", n, x_value1,x_value2);
+        fprintf(file, "%d %d %d %d %d\n", n, x_value1,x_value2,y_value1,y_value2);
     }
 
     fclose(file);
 
-    printf("%d %d",sum1,sum2);
     return 0;
 }
