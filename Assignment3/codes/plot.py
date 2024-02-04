c4c7325a6dc2e710a5acbf8b4bc708a28e5fb45e
venky <venkateshbandawar@gmail.com>
2024-02-04 19:25:28 +0530

    seventh commit

--- Assignment3/codes/plot.py
+++ Assignment3/codes/plot.py
@@ -7,9 +7,11 @@ x_values, y_values = data[:, 0], data[:, 1]
 
 # Plot the graph
 plt.stem(x_values, y_values)
-plt.xlabel('n')
-plt.ylabel('y(n)')
+plt.xlabel('n',fontsize=14)
+plt.ylabel('y(n)',fontsize=14)
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
 plt.grid(True)
-plt.savefig('sumplot.png')
+plt.savefig('sumplot.png',dpi=600)
 plt.show()
 
