1c432921f6a727bab2646dca35af24b4bde7d9d9
venky <venkateshbandawar@gmail.com>
2024-02-18 23:23:04 +0530

    sixth commit

--- Assignment5/codes/values.c
+++ Assignment5/codes/values.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include<math.h>
 
-double calculate_limit() {
+double y_0() {
     double t = 0;  // calculating y(t) at 0+
     return (1 + exp(-t / 5)) * (t >= 0);
+}
+
+double Y_infinity() {
+    double s = 100;  // calculating y(t) at 0+
+    return (1.0 / s) + (1.0 / (s + (1.0 / 5.0)));
 }
 
 int main() {
     FILE *fp;
     fp = fopen("data5.dat", "w");
 
-    for (double t = 0.1; t <= 10.0; t += 0.1) {
-        double Y_t = 1 + exp(-t/5);
-        fprintf(fp, "%.2f %.4f\n", t, Y_t);
+    for (double t = 0.1; t <= 100.0; t += 0.1) {
+        double y_t = 1 + exp(-t/5);
+        double Y_s = (1.0 / t) + (1.0 / (t + (1.0 / 5.0)));
+        fprintf(fp, "%.2f %.4f %.4f\n", t, y_t,Y_s);
     }
 
     fclose(fp);
 
-    double limit_result = calculate_limit();
-    
+    double limit_y = y_0();
+    double limit_Y = Y_infinity();
     return 0;
 }
 
