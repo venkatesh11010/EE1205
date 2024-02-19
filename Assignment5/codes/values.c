3c0ac9daed91bbe7d6d1030e93aa18dbad039ed2
venky <venkateshbandawar@gmail.com>
2024-02-19 21:51:38 +0530

    seventh commit

--- Assignment5/codes/values.c
+++ Assignment5/codes/values.c
@@ -6,25 +6,22 @@ double y_0() {
     return (1 + exp(-t / 5)) * (t >= 0);
 }
 
-double Y_infinity() {
-    double s = 100;  // calculating y(t) at 0+
-    return (1.0 / s) + (1.0 / (s + (1.0 / 5.0)));
-}
-
 int main() {
     FILE *fp;
     fp = fopen("data5.dat", "w");
+    double s = 100;
+    double sY_s = 0.0;
 
     for (double t = 0.1; t <= 100.0; t += 0.1) {
         double y_t = 1 + exp(-t/5);
-        double Y_s = (1.0 / t) + (1.0 / (t + (1.0 / 5.0)));
-        fprintf(fp, "%.2f %.4f %.4f\n", t, y_t,Y_s);
+        sY_s += ((s)*(1 + exp(-t/5))*(exp(-s*t))*(0.1));
+        fprintf(fp, "%.2f %.4f\n", t, y_t);
     }
 
     fclose(fp);
 
     double limit_y = y_0();
-    double limit_Y = Y_infinity();
+    
     return 0;
 }
 
