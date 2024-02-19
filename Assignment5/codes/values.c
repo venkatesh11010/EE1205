1442a7a4ddfd6ac3eccf255bdba256db4800d3c7
venky <venkateshbandawar@gmail.com>
2024-02-20 00:46:16 +0530

    nineth commit

--- Assignment5/codes/values.c
+++ Assignment5/codes/values.c
@@ -2,13 +2,13 @@
 #include<math.h>
 #include <float.h>
 
-double y_t(float t) {
+double y_of_t(float t) {
     return (1 + exp(-t / 5)) * (t >= 0);
 }
 
-double fn_sY_s(float t , double s, double *sY_s) {
-    for (double t = 0.0001; t <= 100.0; t += 0.0001) {
-        *sY_s +=((s)*(y_t(t))*(exp(-s*t))*(0.0001));
+double fn_of_sY_s(float t , double s, double *sY_s) {
+    for (double t = 0.0001; t <= 60.0; t += 0.0001) {
+        *sY_s +=((s)*(y_of_t(t))*(exp(-s*t))*(0.0001));
     }
     return *sY_s;
 }
@@ -21,13 +21,13 @@ int main() {
     double sY_s = 0.0;
 
     for (double t = 0.1; t <= 100.0; t += 0.1) {
-        fprintf(fp, "%e %e\n", t, y_t(t));
+        fprintf(fp, "%e %e\n", t, y_of_t(t));
     }
 
     fclose(fp);
 
     float t_i = 0;
-    printf("%e , %.4f \n",y_t(t_i),fn_sY_s(t,s,&sY_s));
+    printf("%e , %.4f \n",y_of_t(t_i),fn_of_sY_s(t,s,&sY_s));
 
     return 0;
 }
