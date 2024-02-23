0ada8ae43043953b1a0569773d50d7713f9a4b98
venky <venkateshbandawar@gmail.com>
2024-02-24 00:49:41 +0530

    third commit

--- Assignment6/codes/plot.py
+++ Assignment6/codes/plot.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Read data from the dat file
-data = np.loadtxt("data6.dat")
+data = np.loadtxt("data6_1.dat")
 
 # Extract time and y values
 t_values = data[:, 0]
@@ -17,4 +17,29 @@ plt.ylabel('y(t)')
 plt.legend()
 plt.grid(True)
 plt.show()
+plt.clf()
+
+# Read data from the dat file
+data = np.loadtxt("data6_2.dat")
+
+# Extract frequency and phase values
+w_values = data[:, 0]
+A_values = data[:, 1]
+
+# Plot the graph
+plt.plot(w_values, A_values)
+plt.axhline(0, color='black', linewidth=1.5)
+plt.axvline(0, color='black', linewidth=1.5)
+
+tick_positions = [i for i in range(0, 7)]  # Adjust the range as needed
+tick_labels = ['$10^{}$'.format(i) for i in range(0, 7)]
+
+plt.xticks(tick_positions, tick_labels)
+
+plt.xlabel('frequency')
+plt.ylabel('Phase of H')
+plt.legend()
+plt.grid(True)
+plt.show()
+
 
--- Assignment6/codes/values.c
+++ Assignment6/codes/values.c
@@ -5,20 +5,37 @@ double generate_y(double t, double a) {
     return (1 - 2 * exp(-a * t));
 }
 
-int main() {
-    FILE *file = fopen("data6.dat", "w");
+double generate_phase(double w) {
+    return -2 * atan(2 * 3.14 * pow(10, w) / 10000.00);
+}
 
-    if (file == NULL) {
+int main() {
+    FILE *file1 = fopen("data6_1.dat", "w");
+
+    if (file1 == NULL) {
         fprintf(stderr, "Error opening file.\n");
         return 1;
     }
 
     double a = 0.01;
     for (double t = 0; t <= 200; t += 10) {
-        fprintf(file, "%.4f %.4f\n", t, generate_y(t, a));
+        fprintf(file1, "%.4f %.4f\n", t, generate_y(t, a));
     }
 
-    fclose(file);
+    fclose(file1);
+
+    FILE *file2 = fopen("data6_2.dat", "w");
+
+    if (file2 == NULL) {
+        fprintf(stderr, "Error opening file.\n");
+        return 1;
+    }
+
+    for(double w=0 ; w<=6 ; w+=0.01) {
+        fprintf(file2, "%.4f %.4f\n",  w, generate_phase(w));
+    }
+
+    fclose(file2);
 
     return 0;
 }
