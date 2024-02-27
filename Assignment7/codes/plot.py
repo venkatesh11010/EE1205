e9c9df64fda30b5b8afc6deba5580efeaa90f722
venky <venkateshbandawar@gmail.com>
2024-02-27 23:40:32 +0530

    second commit

--- Assignment7/codes/plot.py
+++ Assignment7/codes/plot.py
@@ -6,10 +6,23 @@ data_file = 'data7.dat'
 data = np.loadtxt(data_file)
 
 # Separate values of s and Y(s)
-w_values, bode_mag_values = data[:, 0], data[:, 1]
+w_values, bmvalues1, bmvalues2, bmvalues3, bmvalues4, bmvalues5, bmvalues6, bmvalues7= data[:, 0], data[:, 1], data[:, 2], data[:, 3], data[:, 4], data[:, 5], data[:, 6],data[:, 7]
 
-# Plot the graph
-plt.plot(w_values, bode_mag_values)
+#Plot the graph
+plt.plot(w_values, bmvalues1)
+plt.plot(w_values, bmvalues2)
+plt.plot(w_values, bmvalues3)
+plt.plot(w_values, bmvalues4)
+plt.plot(w_values, bmvalues5)
+plt.plot(w_values, bmvalues6)
+plt.plot(w_values, bmvalues7)
+
+plt.axhline(0, color='black', linewidth=1.5)
+plt.axvline(0, color='black', linewidth=1.5)
+tick_positions = [i for i in range(1, 4)]  # Adjust the range as needed
+tick_labels = ['$10^{}$'.format(i) for i in range(1, 4)]
+
+plt.xticks(tick_positions, tick_labels)
 plt.xlabel('\u03c9')
 plt.ylabel('|L(j\u03c9)|')
 plt.legend()
--- Assignment7/codes/values.c
+++ Assignment7/codes/values.c
@@ -1,17 +1,53 @@
 #include <stdio.h>
-#include<math.h>
+#include <math.h>
 #include <float.h>
 
-double y_of_jw(float w) {
-    return (0.001 * sqrt(100 + pow(w, 2))) / (w * sqrt(4 * pow(w, 2) + 1) * sqrt(0.0001 * pow(w, 2) + 1) * sqrt(pow(w, 2) + 0.01));
+double zero1(double w) {
+    if(log10(w) > 1.0) {
+        return 20.0 * (log10(w) - 1.0);
+    }else{
+        return 0.0;
+    }
+}
+
+double pole1(double w) {
+    if(log10(w) > 0) {
+        return -20.0 * log10(w);
+    }else{
+        return 0.0;
+    }
+}
+
+double pole2(double w) {
+    if(log10(w) > 0.1) {
+        return -20.0 * (log10(w) - 0.1);
+    }else{
+        return 0.0;
+    }
+}
+
+double pole3(double w) {
+    if(log10(w) > -log10(0.5)) {
+        return -20.0 * (log10(w) + log10(0.5));
+    }else{
+        return 0.0;
+    }
+}
+
+double pole4(double w) {
+    if(log10(w) > 2.0) {
+        return -20.0 * (log10(w) - 2.0);
+    }else{
+        return 0.0;
+    }
 }
 
 int main() {
     FILE *fp;
     fp = fopen("data7.dat", "w");
     
-    for (double w = 0.1; w <= 10.0; w += 0.1) {
-        fprintf(fp, "%e %e\n", w, y_of_jw(w));
+    for (double w = 0.1; w <= 1000.0; w += 0.1) {
+        fprintf(fp, "%f %f %f %f %f %f %f %f\n", log10(w), zero1(w), pole1(w), pole2(w), pole3(w), pole4(w), -20.0, zero1(w)+pole1(w)+pole2(w)+pole3(w)+pole4(w)-20.0);
     }
 
     fclose(fp);
