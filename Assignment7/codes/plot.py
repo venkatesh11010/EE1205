76c798427dc22457724c8d830ddd639a7083cb53
venky <venkateshbandawar@gmail.com>
2024-02-29 12:56:04 +0530

    eigth commit

--- Assignment7/codes/plot.py
+++ Assignment7/codes/plot.py
@@ -9,23 +9,26 @@ data = np.loadtxt(data_file)
 w_values, bmvalues1, bmvalues2, bmvalues3, bmvalues4, bmvalues5, bmvalues6, bmvalues7= data[:, 0], data[:, 1], data[:, 2], data[:, 3], data[:, 4], data[:, 5], data[:, 6],data[:, 7]
 
 #Plot the graph
+plt.plot(w_values, bmvalues7, label = 'blue')
 plt.plot(w_values, bmvalues1)
 plt.plot(w_values, bmvalues2)
 plt.plot(w_values, bmvalues3)
 plt.plot(w_values, bmvalues4)
 plt.plot(w_values, bmvalues5)
 plt.plot(w_values, bmvalues6)
-plt.plot(w_values, bmvalues7)
+
 
 plt.axhline(0, color='black', linewidth=1.5)
 plt.axvline(0, color='black', linewidth=1.5)
-tick_positions = [i for i in range(1, 4)]  # Adjust the range as needed
-tick_labels = ['$10^{}$'.format(i) for i in range(1, 4)]
+tick_positions = np.arange(1, 4)
+tick_labels = np.vectorize(lambda i: '$10^{}$'.format(i))(np.arange(1, 4))
+
+plt.xticks(tick_positions, tick_labels)
 
 plt.xticks(tick_positions, tick_labels)
 plt.xlabel('\u03c9')
-plt.ylabel('|L(j\u03c9)|')
-plt.legend()
+plt.ylabel('|T(\u03c9)|')
+plt.legend(['T(\u03c9)'])
 plt.grid(True)
 plt.show()
 
