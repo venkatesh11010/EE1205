8173404116aac85d8311d021dd5f3b275c52c04f
venky <venkateshbandawar@gmail.com>
2024-03-08 00:35:36 +0530

    third commit

--- appendix2/codes/plot.py
+++ appendix2/codes/plot.py
@@ -4,11 +4,32 @@ import matplotlib.pyplot as plt
 # Read data from the dat file
 data = np.loadtxt("data_2.dat")
 
-# Extract frequency and angle of H(s) values
+# Extract frequency and H_db values
 w_values = data[:, 0]
 h_db1_values = data[:, 1]
 h_db2_values = data[:, 2]
 h_db3_values = data[:, 3]
+
+# Plot the graph
+plt.plot(w_values, h_db1_values, label = 'overdamped')
+plt.plot(w_values, h_db2_values, label = 'cridamped')
+plt.plot(w_values, h_db3_values, label = 'underdamped')
+
+plt.axhline(0, color='black', linewidth=1.5)
+plt.axvline(0, color='black', linewidth=1.5)
+plt.xlabel('log\u03C9')
+plt.ylabel('$20log|V_0/V_i|$')
+plt.legend()
+plt.grid(True)
+plt.legend(['overdamped', 'cridamped', 'underdamped'])
+plt.show()
+plt.clf()
+
+# Extract frequency and H_db values
+w_values = data[:, 0]
+h_db1_values = data[:, 4]
+h_db2_values = data[:, 5]
+h_db3_values = data[:, 6]
 
 # Plot the graph
 plt.plot(w_values, h_db1_values, label = 'overdamped')
--- appendix2/codes/values.c
+++ appendix2/codes/values.c
@@ -3,6 +3,26 @@
 #include <float.h>
 #define PI 3.14
 
+double h_db1(double w) {
+    double w_0 = 2000;
+    double p1 = -1000;
+    double p2 = -2000;
+    return (20 * log10(pow(w_0, 2)) - 20 * log10(sqrt(pow(w, 2) + pow(p1, 2))) -20 * log10(sqrt(pow(w, 2) + pow(p2, 2))) )* (w >= 0) ;
+}
+
+double h_db2(double w) {
+    double w_0 = 2000;
+    double p = -1500;
+    return (20 * log10(pow(w_0, 2)) - 20 * log10(pow(w, 2) + pow(p, 2)) )* (w >= 0) ;
+}
+
+double h_db3(double w) {
+    double w_0 = 2000;
+    double w_d = 1950;
+    double sigma = 50;
+    return (20 * log10(pow(w_0, 2)) - 20 * log10(sqrt(pow((-pow(w, 2) + pow(w_d, 2) + pow(sigma, 2)), 2) + pow((2 * sigma * w), 2))) )* (w >= 0) ;
+}
+
 double angle1(double w) {
     double p1 = -1000;
     double p2 = -2000;
@@ -25,7 +45,7 @@ int main() {
     fp = fopen("data_2.dat", "w");
 
     for (double w = 0; w <= 3000; w += 10) {
-        fprintf(fp, "%e %e %e %e\n", w, angle1(w), angle2(w), angle3(w));
+        fprintf(fp, "%e %e %e %e %e %e %e\n", w, h_db1(w), h_db2(w), h_db3(w), angle1(w), angle2(w), angle3(w));
     }
 
     fclose(fp);
